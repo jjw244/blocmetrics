9a8dd572eb3b0e0daffa135717d95b0eb52ad4d0
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-29 22:21:35 -0500

    Api Controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :registered_applications
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
   devise_for :users
   get 'about' => 'welcome#about'
   get 'blocmetrics' => 'welcome#index'
